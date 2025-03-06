b7027f22c674fb741df1a0c04545a6181f31ee06
Fan Van <ya.fanvan@yandex.ru>
2025-03-06 13:15:02 +0300

    no id

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -75,7 +75,7 @@ class PetController {
 
 		Optional<Owner> optionalOwner = this.owners.findById(ownerId);
 		Owner owner = optionalOwner.orElseThrow(() -> new IllegalArgumentException(
-				"Owner not found with id: " + ownerId + ". Please ensure the ID is correct "));
+				"Owner not found with id:  Please ensure the ID is correct "));
 		return owner.getPet(petId);
 	}
 
