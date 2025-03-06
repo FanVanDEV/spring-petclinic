a0244012449fb3a3ab567d3a44cc63de8158cda3
Fan Van <ya.fanvan@yandex.ru>
2025-03-06 13:00:11 +0300

    second

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -61,7 +61,7 @@ class PetController {
 	public Owner findOwner(@PathVariable("ownerId") int ownerId) {
 		Optional<Owner> optionalOwner = this.owners.findById(ownerId);
 		Owner owner = optionalOwner.orElseThrow(() -> new IllegalArgumentException(
-				"Owner was not found with id: " + ownerId + ". Please ensure the ID is correct "));
+				"Owner maybe was found with id: " + ownerId + ". Please ensure the ID is correct "));
 		return owner;
 	}
 
