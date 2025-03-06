38799ca696f19a536182c66508c5dace0f036dbc
Fan Van <ya.fanvan@yandex.ru>
2025-03-06 13:23:14 +0300

    change pet edit endpoint

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -118,7 +118,7 @@ class PetController {
 		return "redirect:/owners/{ownerId}";
 	}
 
-	@GetMapping("/pets/{petId}/edit")
+	@GetMapping("/pets/{petId}/edit/pet")
 	public String initUpdateForm() {
 		return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
 	}
